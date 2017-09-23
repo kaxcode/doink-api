b65c2f7c07945ba06574789493ae855a1d3ae0aa
Kenia Valladarez <keniavalladarez@gmail.com>
2017-09-23 12:38:02 -0400

    Uses passport authentication to reach the api endpoint.

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/todos', 'TodoController');
+Route::get('todos', 'TodoController')->middleware('auth:api');
